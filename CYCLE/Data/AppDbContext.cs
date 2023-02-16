018abe6278a086b958e88eaefb1fefc5966a81d5
kemale777 <@kamalaveliyeva66@gmail.com>
2023-02-16 12:25:16 +0400

    Create

---
+++ CYCLE/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CYCLE.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CYCLE.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+            public DbSet<Banner> Banners { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+    
+}
+}
---
+++ CYCLE/Models/Banner.cs
@@ -0,0 +1,13 @@
+﻿namespace CYCLE.Models
+{
+    public class Banner
+
+    {
+        public int Id { get; set; }
+
+        public string  Title { get; set; }
+
+        public string Content { get; set; }
+        public string PhotoUrl { get; set; }
+    }
+}
